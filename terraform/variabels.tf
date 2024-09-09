58316d1537a52a99ddc8d19f10f53ce6d9661b15
Bernardo Rosa Pinheiro <bedarosa99@gmail.com>
2024-09-09 02:08:18 -0300

    Refactoring terraform project

--- terraform/variabels.tf
+++ terraform/variabels.tf
@@ -1,47 +1,79 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   description = "AWS Region"
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
-variable "eks_cluster_name" {
-  description = "EKS Cluster Name"
-  default     = "my-eks-cluster"
+variable "vpc_name" {
+  description = "Name of the VPC"
+  default     = "my-eks-vpc"
 }
 
-variable "eks_cluster_version" {
-  description = "EKS Cluster Version"
-  default     = "1.21"
-}
-
-variable "vpc_cidr_block" {
-  description = "VPC CIDR Block"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "vpc_azs" {
-  description = "VPC Availability Zones"
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "vpc_private_subnets" {
-  description = "VPC Private Subnets"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "Private Subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "vpc_public_subnets" {
-  description = "VPC Public Subnets"
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  description = "Public Subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
-variable "eks_worker_instance_type" {
-  description = "EKS Worker Instance Type"
-  default     = "t3"
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in VPC"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS support in VPC"
+  type        = bool
+  default     = true
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS Cluster"
+  default     = "my-eks-cluster"
+}
+
+variable "eks_cluster_version" {
+  description = "Version of EKS Cluster"
+  default     = "1.24"
+}
+
+variable "node_group_desired_capacity" {
+  description = "Desired number of nodes in the EKS node group"
+  default     = 2
+}
+
+variable "node_group_min_size" {
+  description = "Minimum number of nodes in the EKS node group"
+  default     = 2
+}
+
+variable "node_group_max_size" {
+  description = "Maximum number of nodes in the EKS node group"
+  default     = 5
+}
+
+variable "node_group_instance_type" {
+  description = "Instance type for EKS node group"
+  default     = "t3.medium"
+}
+
+variable "eip_count" {
+  description = "Number of Elastic IPs for NAT"
+  default     = 1
 }
